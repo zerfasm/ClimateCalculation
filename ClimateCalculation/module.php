fe5aabf191a9595233693a4d30422c0de190dfbc
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-29 08:49:03 +0100

    Update module.php

--- ClimateCalculation/module.php
+++ ClimateCalculation/module.php
@@ -347,14 +347,7 @@ class ClimateCalculation extends IPSModule
         $nr = $this->ReadPropertyString('NameRoom');
         $AID = $this->ReadPropertyString('AlexaID');   
         $AV = $this->ReadPropertyInteger('AlexaVolume');
-	$winopenID = $this->GetIDForIdent('WinOpen'); 
-	$winopen = GetValue($winopenID);
 
-	$wincloseID = $this->GetIDForIdent('WinClose'); 
-	$winclose = GetValue($wincloseID);
-
-	$timewinopenID  = $this->GetIDForIdent('TimeWinOpen'); 
-	$timewinopen = GetValue($timewinopenID);
 	    
 	$wv = $this->ReadPropertyInteger('WindowValue');
 	if ($wv != 0) 
@@ -392,7 +385,14 @@ class ClimateCalculation extends IPSModule
 			
 			if ($winopen > 0)
 			{
+				$winopenID = $this->GetIDForIdent('WinOpen'); 
+				$winopen = GetValue($winopenID);
 
+				$wincloseID = $this->GetIDForIdent('WinClose'); 
+				$winclose = GetValue($wincloseID);
+
+				$timewinopenID  = $this->GetIDForIdent('TimeWinOpen'); 
+				$timewinopen = GetValue($timewinopenID);
 
 				$timediff = ($winclose - $winopen)/60;
 				SetValue($timewinopenID,$timediff);
