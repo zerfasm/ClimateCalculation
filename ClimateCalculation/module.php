085a8088335d451270fa481fc7901984f85a1bb6
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-29 11:10:46 +0100

    Update module.php

--- ClimateCalculation/module.php
+++ ClimateCalculation/module.php
@@ -376,7 +376,7 @@ class ClimateCalculation extends IPSModule
 		{	
 			$this->SetValue('WinClose', IPS_GetVariable($this->ReadPropertyInteger('WindowValue'))["VariableChanged"]);
 			
-				$timediff = ($winclose - $winopen)/60;
+				$timediff = ($this->ReadPropertyString('WinClose') - this->ReadPropertyString('WinOpen'));
 				$this->SetValue('TimeWinOpen',$timediff);
 
 				if ($timewinopen >= 15)
